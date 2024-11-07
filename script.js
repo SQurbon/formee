3ccab4990ee543e85e7df65d168efb9fd072525a
“SQurbon” <qurbonsultonqulov466@gmail.com>
2024-11-07 19:27:05 +0500

    lala

---
+++ script.js
@@ -0,0 +1,63 @@
+// window.addEventListener('load', function () {
+//     var text = this.document.querySelector(".text")
+//     var subtext = text.innerHTML
+//     // text.innerHTML = ""
+//     // console.log(subtext);
+//     var i = "Lorem ipsum dolor sit.!"
+
+//     function writer() {
+//         var interval = this.setInterval(function () {
+//             text.innerHTML = text.innerHTML + subtext[i]
+//             i++
+
+//             if (i >= subtext.length) {
+//                 clearInterval(interval)
+//             }
+//         }, 100)
+
+//     }
+//     writer()
+
+//     // function removetext() {
+//     //     if (i >= subtext.length) {
+//     //         this.setInterval(function () {
+//     //             text.innerHTML - subtext.length
+//     //         }, 100)
+//     //         i--
+//     //     }
+//     // }
+
+//     // removetext()
+// })
+
+
+// ------------------- kiritilgan sonni teskarisiga chiqarish -------------
+
+// var son = prompt('son kiriting') //10
+// var res = 1
+// var i = 1
+// while(res < son){
+//     i = son -= res
+//     document.write(i, "<br/>")
+//     i--
+// }
+
+// ------ daraja -----
+// var son = prompt("son kiriting")
+// var daraja = prompt("darajani kiriting")
+// var res = 1
+// var i = 1
+// while(i <= daraja){
+//     res = res * son
+//     i++
+// }
+// document.write(res)
+
+
+// var son = prompt('son kiriting')
+// var i = 0
+// while(i <= son){
+//    i = son / 3 == 0
+//     i++
+// }
+// document.write(i)
